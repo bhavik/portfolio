ce519cbbb577e31cdc78b62b048920ad640e619f
bhavvik <bhavvik@gmail.com>
2015-05-01 08:38:15 -0500

    Initial Commit

---
+++ portfolio/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  portfolio
+//
+//  Created by Bhavik Shah on 5/1/15.
+//  Copyright (c) 2015 shah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
